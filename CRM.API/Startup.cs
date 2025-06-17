6b95c7d7b15de66683cafa91d7c548c0c03320e4
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-06-17 13:50:02 -0300

    Adiciona configuração de banco de dados e middleware
    - Atualiza `CRM.API.csproj` com novas referências a pacotes NuGet.
    - Refatora `Program.cs` para usar a classe `Startup` para configuração de serviços e pipeline.
    - Adiciona string de conexão no `appsettings.Development.json` para MySQL.
    - Introduz `Startup.cs` com configuração de serviços, middleware e suporte a localização.

---
+++ CRM.API/Startup.cs
@@ -0,0 +1,96 @@
+﻿using CRM.API.Middlewares;
+using CRM.Infrastructure.DbContext;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace CRM.API;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+    private string dadosConexao => Configuration.GetConnectionString("DefaultConnection");
+
+    public void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
+    {
+        services.AddMemoryCache();
+        services.AddControllers()
+            .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null)
+            .AddNewtonsoftJson();
+
+        RegistrarContextos(services, env);
+    }
+
+    private void RegistrarContextos(IServiceCollection services, IWebHostEnvironment env)
+    {
+        services
+           .AddDbContext<CrmDbContext>(options =>
+           {
+               options.UseMySql(dadosConexao,
+                   ServerVersion.Create(
+                       new Version("8.0.28"),
+                       Pomelo.EntityFrameworkCore.MySql.Infrastructure.ServerType.MySql),
+                   m =>
+                   {
+                       m.MigrationsAssembly("Diario.Infrastructure.Migrations");
+                       m.CommandTimeout(50000);
+                   }
+               )
+               .LogTo(s => System.Diagnostics.Debug.WriteLine(s))
+               .EnableDetailedErrors(true)
+               .EnableSensitiveDataLogging(true);
+               options.UseLazyLoadingProxies();
+           });
+    }
+    public void Configure(
+        IApplicationBuilder app,
+        IWebHostEnvironment env)
+    {
+        CultureInfo[] supportedCultures = new[] { new CultureInfo("pt-BR") };
+        app.UseRequestLocalization(new RequestLocalizationOptions
+        {
+            DefaultRequestCulture = new RequestCulture(culture: "pt-BR", uiCulture: "pt-BR"),
+            SupportedCultures = supportedCultures,
+            SupportedUICultures = supportedCultures
+        });
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+
+            //app.UseSwagger();
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Versão única");
+            //    c.RoutePrefix = string.Empty;
+            //    c.OAuthClientId("Diario-Api-Swagger");
+            //    c.OAuthClientSecret("diarioapiswagger-tec1429@");
+            //    c.OAuthAppName("Diario-Api-Swagger");
+            //    c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+            //});
+        }
+
+        //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+        app.Use((context, next) =>
+        {
+            context.Request.Scheme = "https";
+            return next();
+        });
+
+        app.UseRouting();
+        //app.UseAuthentication();
+        //app.UseAuthorization();
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}

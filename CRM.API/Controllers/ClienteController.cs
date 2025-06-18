9f2adee7e4493077a70aa962e4227f79b4e8891b
Nilton Lira - Horus - Suporte RMA <nilttonalvez@gmail.com>
2025-06-18 14:20:43 -0300

    Criando ClienteController

---
+++ CRM.API/Controllers/ClienteController.cs
@@ -0,0 +1,42 @@
+﻿using CRM.Application.DTOs;
+using CRM.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteService _clienteService;   
+        public ClienteController(IClienteService clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> ObterPorId(int id)
+        {
+            var cliente = await _clienteService.ObterPorId(id);
+            if (cliente == null) return NotFound();
+            return Ok(cliente);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Adicionar([FromBody] ClienteDto clienteDto)
+        {
+            if (clienteDto == null) return BadRequest("Cliente não pode ser nulo.");
+            var novoId = await _clienteService.Adicionar(clienteDto);
+            return CreatedAtAction(nameof(ObterPorId), new { id = novoId }, clienteDto);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Atualizar(int id, [FromBody] ClienteDto clienteDto)
+        {
+            if (clienteDto == null) return BadRequest("Cliente não pode ser nulo.");
+            await _clienteService.Atualizar(id, clienteDto);
+            return NoContent();
+        }
+    }
+}

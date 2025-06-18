09a1925493e23b5c05ff6eca8a9480917a18b001
Nilton Lira - Horus - Suporte RMA <nilttonalvez@gmail.com>
2025-06-18 15:26:30 -0300

    Criando endpoints PedidoController

---
+++ CRM.API/Controllers/PedidoController.cs
@@ -0,0 +1,32 @@
+﻿using CRM.Application.DTOs;
+using CRM.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IPedidoService _pedidoService;
+        public PedidoController(IPedidoService pedidoService)
+        {
+            _pedidoService = pedidoService;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> ObterPorId(int id)
+        {
+            var pedido = await _pedidoService.ObterPorId(id);
+            return Ok(pedido);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarPedido([FromBody] PedidoDto pedidoDto)
+        {
+            var novoId = await _pedidoService.CriarPedido(pedidoDto);
+            return CreatedAtAction(nameof(ObterPorId), new { id = novoId }, pedidoDto);
+        }
+    }
+}
